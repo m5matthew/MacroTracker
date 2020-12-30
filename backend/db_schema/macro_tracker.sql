8f98cc1617e51f3266b52f1edb5413415efbc7d1
Matthew Wang <matthew.wang.c@gmail.com>
2020-12-29 16:15:49 -0800

    Added db schema

---
+++ backend/db_schema/macro_tracker.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS entries;
+DROP TABLE IF EXISTS meals;
+
+CREATE TABLE IF NOT EXISTS meals (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR UNIQUE NOT NULL,
+    protein INT DEFAULT 0,
+    fat INT DEFAULT 0,
+    carbs INT DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS entries (
+    date TIMESTAMP NOT NULL,
+    meal_id INT,
+    FOREIGN KEY (meal_id)
+        REFERENCES meals (id)
+);
