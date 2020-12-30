d2b7609e7675896856d1e3a7f6ba83e543115f25
Matthew Wang <matthew.wang.c@gmail.com>
2020-12-29 17:12:49 -0800

    Added eslint and restructured folders

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// set up database pool
+const { Pool } = require('pg')
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
